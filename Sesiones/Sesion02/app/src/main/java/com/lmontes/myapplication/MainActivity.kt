53e764274677fc2372aa9e908e8d4e843bfe3897
strykerluis <lmontes@sweetmedic.com.pe>
2025-02-06 15:18:40 -0500

    creación menú

--- Sesiones/Sesion02/app/src/main/java/com/lmontes/myapplication/MainActivity.kt
+++ Sesiones/Sesion02/app/src/main/java/com/lmontes/myapplication/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.lmontes.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +21,35 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.principal, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.menu_inicio -> {
+                Log.i("===>", "Click en menú inicio")
+                val intent=Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+
+            R.id.menu_producto -> {
+                Log.i("===>", "Click en el menú Productors")
+                val intent= Intent(this, ProductActivity::class.java)
+                startActivity(intent)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+    
+    fun goProductos(view: View){
+        val intent = Intent(this, ProductActivity::class.java)
+        startActivity(intent)
+
     }
 }
